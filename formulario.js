579e09a9c6ef4e287515a0a2b7a00723390964e2
AlexisEV <101740078+AlexisEV@users.noreply.github.com>
2023-04-22 21:55:15 -0500

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,1 +1,12 @@
+function procesarFormulario(event) {
+    event.preventDefault();
+    
+    const formulario = document.forms[0];
+    const data = new FormData(formulario);
 
+    localStorage.setItem("formData", JSON.stringify(Object.fromEntries(data.entries())));
+
+    window.location.href = "resultado.html";
+}
+
+document.querySelector("form").addEventListener("submit", procesarFormulario);
