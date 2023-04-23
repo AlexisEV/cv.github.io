eb965b69d3c039550adb87ea768d4fe19bafc975
AlexisEV <101740078+AlexisEV@users.noreply.github.com>
2023-04-23 01:03:49 -0500

    Update resultado.js

--- resultado.js
+++ resultado.js
@@ -1,12 +1,19 @@
-function mostrarResultado() {
-    const data = JSON.parse(localStorage.getItem("formData"));
-    const resultadoDiv = document.getElementById("resultado");
+document.addEventListener("DOMContentLoaded", function() {
+    const data = JSON.parse(sessionStorage.getItem("formData"));
 
-    for (const [key, value] of Object.entries(data)) {
-        const p = document.createElement("p");
-        p.innerHTML = `<strong>${key}:</strong> ${value}`;
-        resultadoDiv.appendChild(p);
-    }
-}
-
-mostrarResultado();
+    const resultados = document.getElementById("resultados");
+    resultados.innerHTML = `
+        <p><strong>Nombre:</strong> ${data.nombre}</p>
+        <p><strong>Apellidos:</strong> ${data.apellidos}</p>
+        <p><strong>Fecha de nacimiento:</strong> ${data.fnac}</p>
+        <p><strong>Ocupación:</strong> ${data.ocupacion}</p>
+        <p><strong>Teléfono:</strong> ${data.tel}</p>
+        <p><strong>Email:</strong> ${data.email}</p>
+        <p><strong>Nacionalidad:</strong> ${data.nacionalidad}</p>
+        <p><strong>Nivel de inglés:</strong> ${data.nivIngles}</p>
+        <p><strong>Lenguajes de programación:</strong> ${data.lenguajes}</p>
+        <p><strong>Aptitudes:</strong> ${data.aptitudes}</p>
+        <p><strong>Habilidades:</strong> ${data.habilidades}</p>
+        <p><strong>Perfil:</strong> ${data.perfil}</p>
+    `;
+});
